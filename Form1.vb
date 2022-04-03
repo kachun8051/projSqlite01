0b6c9abf6d9a42063a6cffac8c8a17f28a8e47b4
kachun8051 <kachun@prizemart.com>
2022-04-03 17:48:40 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Data.SQLite
+Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim strSql As String = "Create Table If Not Exists highscore"
+        If IO.File.Exists(Application.StartupPath & "\MyDatabase.sqlite") = False Then
+            SQLiteConnection.CreateFile("MyDatabase.sqlite")
+        End If
+    End Sub
+End Class
---
+++ clsDbInit.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.SQLite
+
+Public Class clsDbInit
+
+    Private Const dbName As String = "MyDatabase.sqlite"
+    Private Const connStr As String = "Data Source=" & dbName & "; Version=3;"
+
+    Private conn As SQLiteConnection
+
+
+    Public Function createNewDb() As Boolean
+        Dim dbpath As String = IO.Path.Combine(Application.StartupPath, dbName)
+        Try
+            If IO.File.Exists(dbpath) = False Then
+                SQLiteConnection.CreateFile("MyDatabase.sqlite")
+            End If
+            Return True
+        Catch ex As Exception
+            Diagnostics.Debug.WriteLine("clsDbInit.createNewDb: " & vbCrLf & ex.Message)
+            Return False
+        End Try
+    End Function
+
+
+
+    Private Sub openConn()
+        conn = New SQLiteConnection(connStr)
+        conn.Open()
+    End Sub
+
+    Private Sub closeConn()
+        If conn IsNot Nothing Then
+            conn.Close()
+        End If
+    End Sub
+
+End Class
